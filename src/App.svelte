a6f47fdb923d50250a397c3f79e4f8b1bc234e3d
Shawn Shaw <laoshaw21@gmail.com>
2023-01-22 17:02:04 -0600

    Added sample for client side routes

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import Counter from "./lib/Counter.svelte";
+  import Router from "svelte-spa-router";
+  import routes from "./routes";
 </script>
 
 <main>
+  <!-- Navigation links -->
+  <ul>
+    <li><a href="#/">Home</a></li>
+    <li><a href="#/hello/svelte">Say hi!</a></li>
+    <li><a href="#/wild/card">Wildcard route</a></li>
+    <li><a href="#/does/not/exist">Not found</a></li>
+  </ul>
+
+  <!-- Show the router -->
+  <Router {routes} />
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
       <img src="/vite.svg" class="logo" alt="Vite Logo" />
     </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
@@ -19,14 +31,14 @@
   </div>
 
   <p>
-    Check out <a href="https://github.com/ItalyPaleAle/svelte-spa-router"
-        target="_blank" rel="noreferrer">svelte-spa-router</a> which is the
-    client side router used.
+    Check out <a
+      href="https://github.com/ItalyPaleAle/svelte-spa-router"
+      target="_blank"
+      rel="noreferrer">svelte-spa-router</a
+    > which is the client side router used.
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
