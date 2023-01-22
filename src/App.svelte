1a5192ebaf8b6e721725cece072ff48b02ddee69
Shawn Shaw <laoshaw21@gmail.com>
2023-01-22 17:25:00 -0600

    Using use:link and its disable feature

--- src/App.svelte
+++ src/App.svelte
@@ -2,16 +2,19 @@
   import svelteLogo from "./assets/svelte.svg";
   import Counter from "./lib/Counter.svelte";
   import Router from "svelte-spa-router";
+  import { link } from "svelte-spa-router";
   import routes from "./routes";
 </script>
 
 <main>
   <!-- Navigation links -->
   <ul>
-    <li><a href="#/">Home</a></li>
-    <li><a href="#/hello/svelte">Say hi!</a></li>
-    <li><a href="#/wild/card">Wildcard route</a></li>
-    <li><a href="#/does/not/exist">Not found</a></li>
+    <li><a href="/" use:link>Home</a></li>
+    <li><a href="/hello/svelte" use:link>Say hi!</a></li>
+    <li>
+      <a use:link={{ href: "/wild/card", disabled: true }}>Wildcard route</a>
+    </li>
+    <li><a href="/does/not/exist" use:link>Not found</a></li>
   </ul>
 
   <!-- Show the router -->
