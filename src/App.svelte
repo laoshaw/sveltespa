229c5dca4c59831c96a2880eb7b3415206c4c190
Shawn Shaw <laoshaw21@gmail.com>
2023-01-28 15:07:20 -0600

    Disable all a11y- warnings

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
   import Counter from "./lib/Counter.svelte";
   import Router from "svelte-spa-router";
   import { link } from "svelte-spa-router";
@@ -19,44 +18,8 @@
 
   <!-- Show the router -->
   <Router {routes} />
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
   <div class="card">
     <Counter />
   </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/ItalyPaleAle/svelte-spa-router"
-      target="_blank"
-      rel="noreferrer">svelte-spa-router</a
-    > which is the client side router used.
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
